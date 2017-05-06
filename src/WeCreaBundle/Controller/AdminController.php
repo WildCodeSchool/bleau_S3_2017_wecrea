06b447c4947002215b6e06036ae5f66ce8bb0e16
florenta <florent.alemany@gmail.com>
2017-05-07 00:04:17 +0200

    simplication process

--- src/WeCreaBundle/Controller/AdminController.php
+++ src/WeCreaBundle/Controller/AdminController.php
@@ -210,44 +210,28 @@ class AdminController extends Controller
     }
 
     public function deleteWorkImageAjaxAction(Request $request){
-        /* This method takes in charge the deletion of a single image
-        *  (user action with delete image button) as well as all the images
-        *  created from the beginning if the client decides to stop the
-        *  creation of the artist profile (closes window, goes back to previous page...)
-        */
-
         $em = $this->getDoctrine()->getManager();
+        dump($request);
+        $idWork = $request->request->get('idWork');
+        $work = $em->getRepository('WeCreaBundle:Work')->findOneById($idWork);
 
-        /* If the artist profile has been created */
-        if(!empty($request->request->get('idArt'))){
-            $idArt = $request->request->get('idArt');
-            $artist = $em->getRepository('WeCreaBundle:Artist')->findOneById($idArt);
-        }
-        else{
-            $artist = false;
-        }
-
-        /* Array of images = 1 or all images (if window closed for example) */
         $idImg = $request->request->get('idImg');
 
-        for($i=0; $i<count($idImg); ++$i){
-            $image = $em->getRepository('WeCreaBundle:Images')->findOneById($idImg[$i]);
-            $url = $image->getUrl();
 
-            /* If the artist profile exists, let's remove the images it
-            * is linked with
-            */
-            if($artist !== false){
-                $artist->removeImage($image);
-            }
+        $image = $em->getRepository('WeCreaBundle:Images')->findOneById($idImg);
+        $url = $image->getUrl();
 
-            $path = $this->getParameter('image_directory')."/".$url;
+        /* Let's remove the images the work is linked with
+        */
 
-            if(file_exists($path)){
-                unlink($path);
-            }
-            $em->remove($image);
+        $work->removeImage($image);
+
+        $path = $this->getParameter('image_directory')."/".$url;
+
+        if(file_exists($path)){
+            unlink($path);
         }
+        $em->remove($image);
 
         $em->flush();
 
@@ -268,16 +252,6 @@ class AdminController extends Controller
 
             $image->setUrl($fileName);
             $image->setAlt($alt);
-
-            /* if the artist profile has been created
-            *  before the submission of the new image,
-            *  we can make the association
-            */
-            if(!empty($request->request->get('idArt'))){
-                $idArt = $request->request->get('idArt');
-                $artist = $em->getRepository('WeCreaBundle:Artist')->findOneById($idArt);
-                $artist->addImage($image);
-            }
 
             if(!empty($request->request->get('idWork'))){
                 $idWork = $request->request->get('idWork');
@@ -333,19 +307,6 @@ class AdminController extends Controller
                 $work->setNature($nature);
             }
 
-            //
-            /* If images have been created before the submission
-            *  we just associate them to the work
-            */
-
-            if(!empty($request->request->get('idImg'))){
-                $idImg = $request->request->get('idImg');
-                for($i=0; $i<count($idImg); ++$i){
-                    $image = $em->getRepository('WeCreaBundle:Images')->findOneById($idImg[$i]);
-                    $work->addImage($image);
-                }
-            }
-
             if(!empty($request->request->get('idArt'))){
                 $idArt = $request->request->get('idArt');
                 $artist = $em->getRepository('WeCreaBundle:Artist')->findOneById($idArt);
@@ -365,12 +326,15 @@ class AdminController extends Controller
 
             /* We send back the data regarding the profile */
             $work = $em->getRepository('WeCreaBundle:Work')->findAll();
+            dump($work);
             /* Let's get the last work created */
             $work = $work[count($work)-1];
 
-            $encoders = array(new JsonEncoder());
-            $normalizer = array(new ObjectNormalizer());
-            $serializer = new Serializer($normalizer, $encoders);
+
+            $encoders = new JsonEncoder();
+            $normalizer = new ObjectNormalizer();
+            $normalizer->setIgnoredAttributes(array("artist", "nature"));
+            $serializer = new Serializer(array($normalizer), array($encoders));
 
             $jsonWork = $serializer->serialize($work, "json");
 
