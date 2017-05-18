be62d3c82b1f13768ed80155f81cee6f2b3da390
Archibald0 <dauvergne.fabien@gmail.com>
2017-05-18 16:19:09 +0200

    ADD home client's view ADD Work client's view ADD isotope lib

--- src/WeCreaBundle/Resources/public/js/wecrea.js
+++ src/WeCreaBundle/Resources/public/js/wecrea.js
@@ -4,11 +4,33 @@ $(document).ready( function () {
     $('#moreLink').click( function () {
         $('.container_footer').slideToggle(500);
     });
-    $('.grid').isotope({
-        // options
-        itemSelector: '.grid-item',
+
+// init Isotope
+    var $grid = $('.cards').isotope({
+        itemSelector: '.card',
         layoutMode: 'fitRows'
-    });git 
+    });
+
+
+// filter functions
+    var filterFns = {};
+
+// bind filter button click
+    $('.filters-button-group').on( 'click', 'button', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+// change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });
+    });
+
 });
 
 // Resize work's cards
