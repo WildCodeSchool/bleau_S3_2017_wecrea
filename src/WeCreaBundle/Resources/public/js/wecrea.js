a9a9ab69e7f042a20b834e6b4b782f48bc840c43
Archibald0 <dauvergne.fabien@gmail.com>
2017-05-18 14:14:19 +0200

    save

--- src/WeCreaBundle/Resources/public/js/wecrea.js
+++ src/WeCreaBundle/Resources/public/js/wecrea.js
@@ -4,4 +4,24 @@ $(document).ready( function () {
     $('#moreLink').click( function () {
         $('.container_footer').slideToggle(500);
     });
+    $('.grid').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });git 
+});
+
+// Resize work's cards
+function cardResize(ratio) {
+        var card = $('.cardResizer');
+        var width = parseFloat(card.css('width'));
+        var height = width * ratio;
+        card.css('height', height);
+};
+
+$(document).ready( function () {
+    cardResize(1);
+    $(window).resize(function () {
+        cardResize(1);
+    });
 });
