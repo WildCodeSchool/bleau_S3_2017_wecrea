c6d9452cfb3c56d0917f3f40f9c32ae719e8cf79
Archibald0 <dauvergne.fabien@gmail.com>
2017-07-11 16:35:24 +0200

    Fiw styles and ADD responsives

--- src/WeCreaBundle/Resources/public/js/wecrea.js
+++ src/WeCreaBundle/Resources/public/js/wecrea.js
@@ -102,9 +102,8 @@ $(document).ready( function () {
 
     // Materialize SideNav
     $(".button-collapse").sideNav({
-        menuWidth: 200, // Default is 300
+        menuWidth: 300, // Default is 300
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true, // Choose whether you can drag to open on touch screens,
     });
 
