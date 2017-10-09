8d6205d2e10caea466d6076f9d5a86c77b7fd0a2
Florian Grandjean <florian.pdf@gmail.com>
2017-10-09 13:36:07 +0200

    Fix bug paiement + mail fos

--- src/WeCreaBundle/Controller/UserController.php
+++ src/WeCreaBundle/Controller/UserController.php
@@ -923,7 +923,7 @@ class UserController extends Controller
         $Tva = $em->getRepository('WeCreaBundle:Legal')->findAll();
 
         $tva = number_format($price * $Tva[0]->getTva() / 100, 2);
-        $ttc = number_format(intval($price) + intval($tva), 2);
+        $ttc = number_format(intval($price), 2);
 
         $legal = $Tva[0]->getMention();
 
